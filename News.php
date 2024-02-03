4a465c1a8a2b61251d5c93600adaa07132ca953f
Pr0grammerSimon <szymon@krzywda.eu>
2024-02-03 15:41:40 +0100

    News Update

--- News.php
+++ News.php
@@ -1,4 +1,42 @@
 <?php 
 include('header.html');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="height: 100vh; width: 100vw;">
+    <div class="container h-50">
+        <div class="row g-0 text-center h-100">
+            <div class="col m-3 newsy_glowne rounded-top border-1" style="background-image: url(Images/coding-man.jpg)">
+                <div style="height: 340px;">
+                    <h1 class="pt-4">Newsy</br>Główne</h1>
+                </div>
+                <div class="h-25 bg-light p-0 m-0 rounded-bottom border-1">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
+                </div>
+            </div>
+            <div class="col m-3 newsy_glowne rounded-top border-1" style="background-image: url(Images/coding-man.jpg)">
+                <div style="height: 340px;">
+                    <h1 class="pt-4">Newsy</br>Główne</h1>
+                </div>
+                <div class="h-25 bg-light p-0 m-0 rounded-bottom border-1">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
+                </div>
+            </div>
+            <div class="col m-3 newsy_glowne rounded-top border-1" style="background-image: url(Images/coding-man.jpg)">
+                <div style="height: 340px;">
+                    <h1 class="pt-4">Newsy</br>Główne</h1>
+                </div>
+                <div class="h-25 bg-light p-0 m-0 rounded-bottom border-1">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
 
