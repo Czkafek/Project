170cf283c68c0a60ec317913044a3b87c91ca9a1
Pr0grammerSimon <szymon@krzywda.eu>
2024-02-03 20:40:53 +0100

    News

--- News.php
+++ News.php
@@ -7,36 +7,188 @@ include('header.html');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
 </head>
 <body style="height: 100vh; width: 100vw;">
-    <div class="container h-50">
-        <div class="row g-0 text-center h-100">
-            <div class="col m-3 newsy_glowne rounded-top border-1" style="background-image: url(Images/coding-man.jpg)">
-                <div style="height: 340px;">
-                    <h1 class="pt-4">Newsy</br>Główne</h1>
-                </div>
-                <div class="h-25 bg-light p-0 m-0 rounded-bottom border-1">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
-                </div>
-            </div>
-            <div class="col m-3 newsy_glowne rounded-top border-1" style="background-image: url(Images/coding-man.jpg)">
-                <div style="height: 340px;">
-                    <h1 class="pt-4">Newsy</br>Główne</h1>
-                </div>
-                <div class="h-25 bg-light p-0 m-0 rounded-bottom border-1">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
-                </div>
-            </div>
-            <div class="col m-3 newsy_glowne rounded-top border-1" style="background-image: url(Images/coding-man.jpg)">
-                <div style="height: 340px;">
-                    <h1 class="pt-4">Newsy</br>Główne</h1>
-                </div>
-                <div class="h-25 bg-light p-0 m-0 rounded-bottom border-1">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
-                </div>
-            </div>
-        </div>
+<div class="container">
+<div class="row row-cols-1 row-cols-lg-3 g-4 mx-auto my-5">
+  <div class="col">
+    <div class="card h-100">
+      <img src="Images/coding-man.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Last updated 3 mins ago</small>
+      </div>
     </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="Images/Tlo2.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="Images/pomalowane-na-szaro-gladkie-teksturowane-tlo.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. </p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="row row-cols-2 row-cols-md-2 g-4 mx-auto">
+  <div class="col">
+    <div class="card">
+      <img src="Images/pomalowane-na-szaro-gladkie-teksturowane-tlo.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="Images/coding-man.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row sticky-top float-end d-none d-lg-block my-3 w-25">
+    <div class="col-12">
+        <h3 class="text-light" style="padding: 0%; margin: 0%;">Na czasie</h3>
+        <img class="d-inline" src="Images/Line 1.png">
+        <form class="form-inline my-2">
+        <input class="form-control mr-sm-2" type="email" placeholder="Mail" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Zapisz się</button>
+    </form>
+    </div>
+</div>
+
+<div class="card mb-3 my-5 mx-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-4">
+      <img src="Images/coding-man.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3 my-3 mx-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-4">
+      <img src="Images/coding-man.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="card mb-3 my-3 mx-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-4">
+      <img src="Images/coding-man.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="card mb-3 my-3 mx-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-4">
+      <img src="Images/coding-man.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="card mb-3 my-3 mx-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-4">
+      <img src="Images/coding-man.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="card mb-3 my-3 mx-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-4">
+      <img src="Images/coding-man.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="card mb-3 my-3 mx-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-4">
+      <img src="Images/coding-man.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="card mb-3 my-3 mx-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-4">
+      <img src="Images/coding-man.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
 </body>
 </html>
 
