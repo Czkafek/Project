70cfcf37decf8a9bb4a0fbcd012efbf72be597dc
Pr0grammerSimon <szymon@krzywda.eu>
2024-02-04 14:59:24 +0100

    Back-End of News

--- Newsy.php
+++ Newsy.php
@@ -1,5 +1,6 @@
 <?php 
 include('header.html');
+include_once('database.php')
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,16 +15,21 @@ include('header.html');
 <div class="row row-cols-1 row-cols-lg-3 g-4 mx-auto my-5">
   <?php
     for ($i=0; $i<3; $i++){
+      $news = $i;
+      $news_k = $news+1;
+      $query = "SELECT * FROM `News` ORDER BY `DateTime` DESC LIMIT $news,$news_k ;";
+      $result = mysqli_query($conn,$query);
+      $row = mysqli_fetch_assoc($result);
     echo '<div class="col">';
-    echo  '<a href="" style="text-decoration: none;">';
+    echo  '<a href="strona_news.php?news='.$i.'" style="text-decoration: none;">';
     echo '<div class="card h-100 border-0">';
-    echo'<img src="Images/coding-man.jpg" class="card-img-top" alt="...">';
+    echo'<img src="'.$row['Image'].'" class="card-img-top" alt="...">';
     echo  '<div class="card-body">';
-    echo'    <h5 class="card-title">Card title</h5>';
-    echo'    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>';
+    echo'    <h5 class="card-title">'.$row['Title'].'</h5>';
+    echo'    <p class="card-text">'.$row['Short'].'</p>';
     echo '</div>';
     echo  '<div class="card-footer">';
-    echo'    <small class="text-body-secondary">Last updated 3 mins ago</small>';
+    echo'    <small class="text-body-secondary">'.$row['DateTime'].'</small>';
     echo'  </div>';
     echo'</div>';
     echo'</a>';
@@ -94,7 +100,7 @@ include('header.html');
   </div>
 </div>
 <?php
-for ($i=0;$i<5;$i++){
+for ($i=0;$i<4;$i++){
 echo'<div class="card mb-3 my-3 mx-3" style="max-width: 540px;">';
 echo'  <div class="row g-0">';
 echo'    <div class="col-4">';
@@ -103,7 +109,7 @@ echo'    </div>';
 echo'    <div class="col-8">';
 echo'      <div class="card-body">';
 echo'        <h5 class="card-title">Card title</h5>';
-echo'        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>';
+echo'        <p class="card-text">'.$i.'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>';
 echo'      </div>';
 echo'    </div>';
 echo'  </div>';
---
+++ strona_news.php
@@ -0,0 +1,30 @@
+<?php
+    include_once('database.php')
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $news = $_GET['news'];
+    $news_k = $news+1;
+    $query = "SELECT * FROM `News` ORDER BY `DateTime` DESC LIMIT $news,$news_k ;";
+    $result = mysqli_query($conn,$query);
+    $row = mysqli_fetch_assoc($result);
+    echo $row['Title'];
+
+
+    // echo $_GET['news'].'<br>';
+    // echo time().'<br>';
+    // $data = New DateTime();
+    // var_dump($data);
+    // echo $data->format('m/d/Y');
+
+    ?>
+</body>
+</html>
+
