27577bf487af1a6c466f1cbf2511b15eae311188
Czkafek <czkafek@gmail.com>
2024-03-02 16:03:38 +0100

    lol

---
+++ edit_profile.php
@@ -0,0 +1,28 @@
+<?php 
+
+    include 'navbar.html';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+
+        
+
+    </main>
+</body>
+</html>
+
+
+<?php 
+
+    include 'footer.html';
+
+?>
